70c75394499879ecfcc7f3f262fa57a12c43eed8
James Nicol <james.andrew.nicol@gmail.com>
2014-10-07 10:59:44 -0700

    Added an examples page

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var gulp      = require('gulp'),
-    util      = require('gulp-util'),
-    minify    = require('gulp-minify-css'),
-    rename    = require('gulp-rename'),
-    atomicity = require('./');
+var gulp       = require('gulp'),
+    util       = require('gulp-util'),
+    minify     = require('gulp-minify-css'),
+    rename     = require('gulp-rename'),
+    animations = require('./');
 
 gulp.task('css', function() {
-  atomicity.gulp
+  animations.gulp
     .src({
       minify: false,
       autoprefixer: true
     })
     .pipe(gulp.dest('.'))
-    .pipe(rename(atomicity.file.replace('.css', '.min.css')))
+    .pipe(rename(animations.file.replace('.css', '.min.css')))
     .pipe(minify())
     .pipe(gulp.dest('.'));
 });
@@ -25,9 +25,11 @@ gulp.task('css:watch', ['css'], function(){
 /**
 Development server and livereload for the sample app
 */
-var opnr = require('open'),
-    connect = require('gulp-connect'),
-    port = 4444;
+var opnr      = require('open'),
+    connect   = require('gulp-connect'),
+    fs        = require('fs'),
+    atomicity = require('atomicity.css'),
+    port      = 4444;
 
 gulp.task('server', function() {
   connect.server({
@@ -38,9 +40,21 @@ gulp.task('server', function() {
   opnr('http://localhost:' + port);
 });
 
-gulp.task('watch', ['css:watch','server'], function(){
+gulp.task('css:dev', function(){
+  if (!fs.existsSync('./dist')){
+    fs.mkdirSync('./dist');
+  }
+  atomicity.gulp.src({autoprefixer:true})
+    .pipe(animations.gulp.through({autoprefixer:true}))
+    .pipe(gulp.dest('dist/'));
+});
+gulp.task('css:dev:watch', ['css:dev'], function(){
+  gulp.watch(['css/**/*.scss'], ['css']);
+});
+
+gulp.task('watch', ['css:dev:watch','server'], function(){
   gulp.watch(
-    ['./*.html', 'css/*.scss'],
+    ['./*.html', './dist/*.css'],
     function(event){
       util.log('file changed:', util.colors.green(event.path));
       gulp.src(event.path)
